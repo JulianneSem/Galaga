190ba1ac5151130deab9836a7a10ddd198424f71
semj <semj@wwu.edu>
2020-09-01 14:39:17 -0700

    started adding enemies and projectiles, need to clean up code readability

--- space_invaders.py
+++ space_invaders.py
@@ -25,6 +25,25 @@ class Player(pygame.sprite.Sprite):
         self.rect.move_ip(1, 0)
         if self.rect.right > 800:
             self.rect.right = 800
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 0, 255))
+        self.rect = self.surf.get_rect(center=(50,50))
+        # self.speed = random.randint(5, 20)
+
+class Projectile(pygame.sprite.Sprite):
+    def __init__(self,startX, startY):
+        super(Projectile, self).__init__()
+        self.surf = pygame.Surface((3, 10))
+        self.surf.fill((255, 0, 255))
+        self.rect = self.surf.get_rect(center=(startX,startY))
+        self.speed = -1
+    def update(self):
+        self.rect.move_ip(0, self.speed)
+        if self.rect.right < 0:
+            self.kill()
 
 pygame.init()
 
@@ -35,8 +54,20 @@ screen.fill((255, 255, 255))
 player = Player()
 pygame.display.flip()
 
+enemies = pygame.sprite.Group()
+projectiles = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+
 running = True
+wonRound = True
 while running:
+    if wonRound == True:
+        enemy = Enemy()
+        enemies.add(enemy)
+        all_sprites.add(enemy)
+        wonRound = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -48,7 +79,14 @@ while running:
                 player.moveLeft()
             if event.key == K_RIGHT:
                 player.moveRight()
-    screen.blit(player.surf, player.rect)
+            if event.key == K_UP:
+                projectile = Projectile(player.rect.left + ((player.rect.right - player.rect.left)/2), player.rect.top)
+                all_sprites.add(projectile)
+                projectiles.add(projectile)
+    for pro in projectiles:
+        pro.update()
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
     pygame.display.flip()
     screen.fill((255, 255, 255))
 pygame.quit()
